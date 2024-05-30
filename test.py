f35c2ec8b7b4607071ff1ba67bfda6fa9e5f993d
Ricardo <ricar.d1997@hotmail.com>
2024-05-30 17:55:57 -0400

    add test file

---
+++ test.py
@@ -0,0 +1,11 @@
+import MDAnalysis as mda
+import sys
+sys.path.append("/projects/academic/vmonje/ricardox/github/analysis")
+from analysis_class import analysis
+
+
+
+top = "/projects/academic/vmonje/ricardox/trajectories"
+
+
+
