8af66ebcafae599ea3d010f5d870d1d3e1adb418
Antonio Bosch <antonio.bosch@uam.es>
2024-11-25 14:44:31 +0100

    Start Hbond Analysis

--- Tests_Antonio.py
+++ Tests_Antonio.py
@@ -6,18 +6,28 @@ import pandas as pd
 from protein2D_analysis import protein2D_analysis
 
 trj_path='/home/antonio/Desktop/VIRMAT/Paper_PB_KDE/SIMs/RBD-PBLs_wGlyc_closed_layed/glyc_head/rep1/omicron_0/'
+u=mda.Universe(f"{trj_path}md_0_1.tpr",f"{trj_path}md_short_compact.xtc")
 
-u=mda.Universe(f"{trj_path}md_0_1.gro",f"{trj_path}md_short_compact.xtc")
-
-sel = u.select_atoms("protein and resid 34-45")
+trj_path2='/home/antonio/Desktop/VIRMAT/Paper_PB_KDE/SIMs/RBD-PBLs_wGlyc_closed_layed/glyc_head/rep1/omicron_0/'
+u2=mda.Universe(f"{trj_path}md_0_1.tpr",f"{trj_path}md_short_compact.xtc")
+print(u.trajectory.timeseries())
+sel = u.select_atoms("resid 193-200 or protein")
 ag_analysis = protein2D_analysis(sel)
 print(np.shape(ag_analysis.atom_group.atoms))
-pos=ag_analysis.getPositions()
+ag_analysis.getPositions()
 print(ag_analysis.pos.shape)
-# pos=ag_analysis.getCOMs()
-# print(ag_analysis.com.shape)
-ag_analysis.system_name='Omicron PBL0'
 
+
+
+sel2 = u2.select_atoms("resid 193-200 or protein")
+ag_analysis2 = protein2D_analysis(sel2)
+print(np.shape(ag_analysis2.atom_group.atoms))
+ag_analysis2.getPositions()
+print(ag_analysis2.pos.shape)
+
+ag_analysis_sum=ag_analysis+ag_analysis2
+ag_analysis.system_name='Omicron PBL0'
+print(ag_analysis_sum.universe)
 ########### TEST GENERAL MODULES #############
 zlim=40
 Nframes=200
@@ -35,8 +45,13 @@ Nframes=200
 
 # ##########TEST Contour PLOTS ################
 
-paths=ag_analysis.getKDEAnalysis(zlim,Nframes)
+# paths=ag_analysis.getKDEAnalysis(zlim,Nframes)
+# ag_analysis.plotPathsInLvl(1)
+# areas=ag_analysis.getAreas(2,getTotal=True)
+# print(areas)
 
-ag_analysis.plotPathsInLvl(1)
+##### TEST HBONDS PLOTS #####
+ag_analysis.getHbonds('protein','resid 192-193', update_selections=False,trj_plot=True)
+
 
 
