924ac0131e20c14009bca629d8bfa438340ac67d
Antonio Bosch <antonio.bosch@uam.es>
2024-11-26 13:54:17 +0100

    added plotHbondPerResidues

--- Tests_Antonio.py
+++ Tests_Antonio.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from protein2D_analysis import protein2D_analysis
+from protein2D_analysis import plotPathsInLevel
 
 trj_path='/home/antonio/Desktop/VIRMAT/Paper_PB_KDE/SIMs/RBD-PBLs_wGlyc_closed_layed/glyc_head/rep1/omicron_10/'
 u=mda.Universe(f"{trj_path}md_0_1.tpr",f"{trj_path}md_short_compact.xtc")
-sel = u.select_atoms("resid 193-200 or protein")
+sel = u.select_atoms("resid 193-200 or resname DOL or protein")
 ag_analysis = protein2D_analysis(sel)
 ag_analysis.getPositions()
 print(ag_analysis.pos.shape)
@@ -30,35 +31,20 @@ Nframes=200
 
 # ##########TEST Contour PLOTS ################
 
-paths=ag_analysis.getKDEAnalysis(zlim,Nframes)
+# paths=ag_analysis.getKDEAnalysis(zlim,Nframes)
 # ag_analysis.plotPathsInLvl(1)
 # areas=ag_analysis.getAreas(2,getTotal=True)
 # print(areas)
 
 ##### TEST HBONDS PLOTS #####
-ag_analysis.getHbonds('protein','resid 193-200', update_selections=False,trj_plot=False)
-df=ag_analysis.HbondsPerResidues()
-print(df)
 
-max_val=df['Count'].max()
-str_resids=' '.join(np.array(df['ResIDs'],dtype=str))
-print(str_resids)
-res_w_hbonds=u.select_atoms(f'resid {str_resids}')
-ag_w_hbonds=protein2D_analysis(res_w_hbonds)
-ag_w_hbonds.getPositions()
-ag_analysis.plotPathsInLvl(0)
-ag_analysis.plotPathsInLvl(5)
-ag_analysis.plotPathsInLvl(8)
-for i in range(ag_w_hbonds.pos.shape[1]):
-    norm_val=df['Count'].iloc[i]/len(ag_w_hbonds.universe.trajectory) #max_val
-    norm_val_plot=df['Count'].iloc[i]/max_val
-    pos=ag_w_hbonds.pos[:,i].mean(axis=0)
-    plt.plot(pos[1],pos[3], 'o',
-            label='%s-%s (%.2f)'%(ag_w_hbonds.atom_group.residues.resids[i],
-                                  ag_w_hbonds.atom_group.residues.resnames[i], norm_val*100),)
-    plt.scatter(pos[1],pos[3], s=(8*20*norm_val_plot)**2, alpha=.5)
-plt.legend()
-plt.show()
+sel_for_path = u.select_atoms("resid 193-200")
+ag_for_path = protein2D_analysis(sel_for_path)
+ag_for_path.getPositions()
+paths=ag_for_path.getKDEAnalysis(zlim,Nframes)
+ag_analysis.getHbonds('resname DOL','resid 193-200', update_selections=False,trj_plot=False)
+ag_analysis.plotHbondsPerResidues(paths,contour_lvls_to_plot=[0,5,8],top=5, print_table=True)
+# ag_analysis.plotHbondsPerResidues(paths,contour_lvls_to_plot=None, print_table=True)
 
 
 
