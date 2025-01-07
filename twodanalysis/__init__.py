56d9e41662b1baa4ecf83cbafd97587dc1e469f0
Ricardo Ramirez <ricar.d1997@hotmail.com>
2025-01-07 17:57:03 -0500

    change init delete memb2d

--- twodanalysis/__init__.py
+++ twodanalysis/__init__.py
@@ -6,7 +6,6 @@ Project created to study lipid membranes in a 2D fashion.
 # Add imports here
 from importlib.metadata import version
 from .BioPolymer2D import BioPolymer2D
-from .Memb2D import Memb2D
 from .MembProp import MembProp
 from .Cumulative2D import Cumulative2D
 from .Voronoi2D import Voronoi2D
