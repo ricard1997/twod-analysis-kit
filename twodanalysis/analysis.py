72354eb092b1caa7c5014caa71ea4ed51acd2bed
Ricardo Ramirez <ricar.d1997@hotmail.com>
2025-01-08 15:58:43 -0500

    add order parameters for a selection

--- twodanalysis/analysis.py
+++ twodanalysis/analysis.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from twodanalysis import MembProp
 
 class OrderParameters:
 
@@ -140,6 +140,7 @@ class OrderParameters:
         -----
         The return is a vector containing the value :math:`\braket{cos^2(\theta_i}`. As follows:
 
+
         .. math:: [\braket{cos^2(\theta_2}, \braket{cos^2(\theta_3}, ..., \braket{cos^2(\theta_{n_chain}}]
 
         The index starts at 2 because that is the carbon the lipid tail starts with.
@@ -185,7 +186,53 @@ class OrderParameters:
         chains = np.array(chains) # Expect array of dim (n_chain, n_lipids)
         return chains
 
+    def _check_one_lipid(atoms):
+        resnames = list(set(atoms.residues.resnames))
+        if len(resnames) != 1:
+            raise "This function only works for one lipid at a time. Please make sure you use only one lipid"
+        return resnames[0]
+
+    @classmethod
+    def scd(self,universe, selection, chain = "sn1",
+             n_chain = None,
+             start = 0,
+             final = -1,
+             step = 1,
+             ):
+
+        print(type(universe))
+
+        lipids = universe.select_atoms(selection)
+        lipid_name = self._check_one_lipid(lipids)
+
+
+        if n_chain is None:
+            membrane =  MembProp(lipids)
+            membrane.guess_chain_lenght()
+            chain_info = membrane.chain_info
+            if chain == "sn1":
+                n_chain = chain_info[lipid_name][0]
+            elif chain == "sn2":
+                n_chain = chain_info[lipid_name][1]
+            else:
+                raise "This code only support sn1 and sns as chains"
+
+
+        orders = []
+        for _ in universe.trajectory[start:final:step]:
+            if chain == "sn1":
+                order_data = self.individual_order_sn1(lipids,lipid_name,n_chain)
+            elif chain == "sn2":
+                order_data = self.individual_order_sn2(lipids,lipid_name,n_chain)
+
+            orders.append(np.mean(order_data, axis = 1))
+        orders = np.array(orders)
+        final = np.abs(1.5 * np.mean(orders, axis = 0) - 0.5)
+        return final
 
 
 
 
+
+
+#class SplayAngle:
