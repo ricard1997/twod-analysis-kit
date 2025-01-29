3bbbecc019cd371aacfa6ed081baaa21fc398b9d
vmonje <vmonjeb@gmail.com>
2025-01-29 02:55:41 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,8 +116,8 @@ html_theme_options = {
 
 # Set your logo and favicon here -- replace the placeholders!
 # An SVG with empty gears is provided for editing
-html_logo = "_static/logo/mdakits-placeholder-logo.png"
-html_favicon = "_static/logo/mdakits-empty-favicon-template.svg"
+html_logo = "_static/logo/temp-logo-2Danalysis.png"
+html_favicon = "_static/logo/temp-logo-2Danalysis.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
