f368863b02d09bc27a3c47b473964582ed916d33
Antonio Bosch <antonio.bosch@uam.es>
2025-01-31 10:46:51 +0100

    Update Test script Biopolymer2D

--- twodanalysis/tests/Tests_BioPolymer2D.py
+++ twodanalysis/tests/Tests_BioPolymer2D.py
@@ -17,7 +17,7 @@ ag_analysis = BioPolymer2D(sel, surf_selection='resname DOL and name O1 and prop
 ag_analysis.system_name='Omicron PBL1'
 # ag_analysis.startT=100
 ag_analysis.getPositions()
-
+print(ag_analysis.surf_pos, 'surface position')
 zlim=15
 Nframes=300
 
@@ -31,7 +31,8 @@ print('############# TEST POLAR ANALYSIS ############')
 select_res='resid 198 200 12 8 40 45 111 115 173'
 fig, ax = plt.subplots(1, 1, subplot_kw={'projection': 'polar'}, figsize=(12, 12))
 hist_arr,pos_hist=ag_analysis.PolarAnalysis(select_res,Nframes,# sort=[1,2,3,4,5,6,7,8,0],
-                                            zlim=zlim,control_plots=False,plot=True)
+                                            zlim=zlim,control_plots=False,plot=True,ax=ax)
+plt.show()
 # print(hist_arr.shape,pos_hist.shape)
 
 print('############# TEST RADII OF GYRATION ANALYSIS ########')
@@ -42,11 +43,13 @@ plt.show()
 ag_analysis.RgPerpvsRgsPar(rgs, 'tab:green',show=True)
 
 print('# ##########TEST CONTOUR PLOTS ################')
-### CHECK
-paths=ag_analysis.getKDEAnalysis(zlim,Nframes)
-print(ag_analysis.kdeanalysis.kde)
+sel_contact = u.select_atoms("protein and (resid 4-15 or resid 34-45 or resid 104-117 or resid 170-176)")
+contact_reg = BioPolymer2D(sel_contact, surf_selection='resname DOL and name O1 and prop z > 16')
+contact_reg.getPositions()
+paths=contact_reg.getKDEAnalysis(zlim,Nframes)
+
 fig, ax = plt.subplots(figsize=(6.5,6.5))
-ag_analysis.plotPathsInLevel(paths,1,show=False,ax=ax)
+contact_reg.plotPathsInLevel(paths,0,show=False,ax=ax)
 areas=BioPolymer2D.getAreas(paths,2,getTotal=True)
 print(areas)
 ag_analysis.KDEAnalysisSelection('resid 198 200 12 8 40 45 111 115 173',Nframes,zlim,legend=True,ax=ax)
@@ -55,7 +58,7 @@ plt.show()
 print('##### TEST HBONDS PLOTS #####')
 
 sel_for_path = u.select_atoms("resid 193-200")
-ag_for_path = BioPolymer2D(sel_for_path)
+ag_for_path = BioPolymer2D(sel_for_path,surf_selection='resname DOL and name O1 and prop z > 16')
 ag_for_path.getPositions()
 paths=ag_for_path.getKDEAnalysis(zlim,Nframes)
 ag_analysis.getHbonds('resname DOL','protein or resid 193-200', update_selections=False,trj_plot=False)
